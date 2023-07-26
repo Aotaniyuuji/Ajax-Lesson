cc7b292a054ede5649af91f434ed213c1a874d55
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-07-26 07:41:56 +0000

    [update]Ajax学習

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-$(function() {
-  $('#back a').on('click',function(event){
-    $('body, html').animate({
-      scrollTop:0
-    }, 800);
-    event.preventDefault();
+$(function(){
+  $('#btn').on('click', function() {
+
   });
 });
